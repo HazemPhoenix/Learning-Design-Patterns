e62667b64d484ee6d7197017138ce581b26f7421
Hazem Abdulmoneim <hazem.mando408@gmail.com>
2024-09-02 11:30:32 +0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,148 @@
+﻿namespace DesignPatterns
+{
+    internal class Program
+    {
+
+        interface IFlyBehavior
+        {
+            public void Fly();
+        }
+
+        interface IQuackBehavior
+        {
+            public void Quack();
+        }
+
+        class FlyWithWings : IFlyBehavior
+        {
+            public void Fly()
+            {
+                Console.WriteLine("Flying with wings!");
+            }
+        }
+
+        class FlyNoWay : IFlyBehavior
+        {
+            public void Fly()
+            {
+                Console.WriteLine("Tries to fly but can't :( ");
+            }
+        }
+
+        class NormalQuack : IQuackBehavior
+        {
+            public void Quack ()
+            {
+                Console.WriteLine("Quacks!");
+            }
+        }
+        class Squeak : IQuackBehavior
+        {
+            public void Quack ()
+            {
+                Console.WriteLine("Squeaks!");
+            }
+        }
+        class MuteQuack : IQuackBehavior
+        {
+            public void Quack ()
+            {
+                Console.WriteLine("Quacks in silence!");
+            }
+        }
+        
+        class RoboticQuack : IQuackBehavior
+        {
+            public void Quack ()
+            {
+                Console.WriteLine("Quacks in a robotic voice!");
+            }
+        }
+
+        class RoboticFly : IFlyBehavior
+        {
+            public void Fly ()
+            {
+                Console.WriteLine("Flies using artificial wings!");
+            }
+        }
+
+        class Duck
+        {
+            public IQuackBehavior quackBehavior;
+            public IFlyBehavior flyBehavior;
+
+            public void PerformQuack()
+            {
+                quackBehavior.Quack();
+            }
+            public void PerformFly()
+            {
+                flyBehavior.Fly();
+            }
+
+        }
+
+
+        class MallardDuck : Duck
+        {
+            public MallardDuck ()
+            {
+                quackBehavior = new NormalQuack();
+                flyBehavior = new FlyWithWings();
+                
+            }
+        }
+
+        class RubberDuck : Duck
+        {
+            public RubberDuck ()
+            {
+                quackBehavior = new Squeak();
+                flyBehavior = new FlyNoWay();
+            }
+        }
+
+        class WoodenDuck : Duck
+        {
+            public WoodenDuck ()
+            {
+                quackBehavior = new MuteQuack();
+                flyBehavior = new FlyNoWay();
+            }
+        }
+
+        class RobotDuck : Duck
+        {
+            public RobotDuck ()
+            {
+                quackBehavior = new RoboticQuack();
+                flyBehavior = new RoboticFly();
+            }
+        }
+
+
+
+
+        static void Main(string[] args)
+        {
+            Duck WoodenDuck = new WoodenDuck();
+            Duck RubberDuck = new RubberDuck();
+            Duck MallardDuck = new MallardDuck();
+            Duck RobotDuck = new RobotDuck(); 
+
+
+            WoodenDuck.PerformQuack();
+            WoodenDuck.PerformFly();
+
+            RubberDuck.PerformQuack();
+            RubberDuck.PerformFly();
+
+            MallardDuck.PerformQuack();
+            MallardDuck.PerformFly();
+
+            RobotDuck.PerformQuack();
+            RobotDuck.PerformFly();
+        }
+    }
+}
