292620aa73302c5bce69e1227b52b78d764d1c0a
Hazem Abdulmoneim <hazem.mando408@gmail.com>
2024-09-02 12:44:39 +0300

    added 4 concrete strategies for the puzzel

--- StrategyPuzzel.cs
+++ StrategyPuzzel.cs
@@ -6,7 +6,49 @@ using System.Threading.Tasks;
 
 namespace DesignPatterns
 {
-    internal class StrategyPuzzel
+    
+    // The strategy interface
+    interface IWeaponBehavior
     {
+        public void UseWeapon();
     }
+
+    // Concrete strategies
+
+    class KnifeBehavior : IWeaponBehavior
+    {
+        public void UseWeapon()
+        {
+            Console.WriteLine("Stabs!");
+        }
+    }
+
+    class SwordBehavior : IWeaponBehavior
+    {
+        public void UseWeapon()
+        {
+            Console.WriteLine("Slashes!");
+        }
+    }
+
+    class AxeBehavior : IWeaponBehavior
+    {
+        public void UseWeapon ()
+        {
+            Console.WriteLine("Chops!");
+        }
+    }
+
+    class BowAndArrowBehavior : IWeaponBehavior
+    {
+        public void UseWeapon()
+        {
+            Console.WriteLine("Shoots!");
+        }
+    }
+
+    
+    
+    
+
 }
